5d1f76aaa180233901b28905cf530c06870aeb6d
王自强 <814120507@qq.com>
2018-05-28 19:59:33 +0800

    jest好像有内存限制，使用node直接运行用例

--- __test__/pinyin.test.js
+++ __test__/pinyin.test.js
@@ -1,6 +1,7 @@
 const pinyin = require('../src/index');
 
 const cases = [
+
   ['空', '', {
     STYLE_NORMAL: [],
     STYLE_TONE: [],
@@ -8,12 +9,6 @@ const cases = [
     STYLE_FIRST_LETTER: []
   }],
   ['单音字', '我', {
-    STYLE_NORMAL: ['wo'],
-    STYLE_TONE: ['wǒ'],
-    STYLE_TONE_NUM: ['wo3'],
-    STYLE_FIRST_LETTER: ['w']
-  }],
-  ['黑糊糊', '黑糊糊', {
     STYLE_NORMAL: ['wo'],
     STYLE_TONE: ['wǒ'],
     STYLE_TONE_NUM: ['wo3'],
@@ -68,24 +63,43 @@ const cases = [
     STYLE_FIRST_LETTER: ['z', 'g']
   }, {only_chinese: true}],
 ];
+//
+// function createTest(desc, words, styles, options = {}) {
+//   describe(desc, () => {
+//     Object.keys(styles).forEach((item, index) => {
+//       test(String.fromCharCode(97 + index) + '.' + item, () => {
+//         expect(pinyin(words, Object.assign({}, {style: pinyin[item]}, options))).toEqual(styles[item]);
+//       });
+//     });
+//   });
+// }
+//
+// describe('pinyin的测试用例', () => {
+//   describe('非字符串输入', () => {
+//     test('1、数字', () => {
+//       expect(pinyin.bind(null, 12)).toThrow();
+//     });
+//   });
+//   cases.forEach((item, index) => {
+//     createTest(index + 1 + '.' + item[0], item[1], item[2], item[3]);
+//   });
+// });
 
-function createTest(desc, words, styles, options = {}) {
-  describe(desc, () => {
-    Object.keys(styles).forEach((item, index) => {
-      test(String.fromCharCode(97 + index) + '.' + item, () => {
-        expect(pinyin(words, Object.assign({}, {style: pinyin[item]}, options))).toEqual(styles[item]);
-      });
-    });
-  });
-}
+var colors = require( 'colors');
 
-describe('pinyin的测试用例', () => {
-  describe('非字符串输入', () => {
-    test('1、数字', () => {
-      expect(pinyin.bind(null, 12)).toThrow();
-    });
-  });
-  cases.forEach((item, index) => {
-    createTest(index + 1 + '.' + item[0], item[1], item[2], item[3]);
+cases.forEach((item, index) => {
+  const desc = item[0];
+  const words = item[1];
+  const styles = item[2];
+  const options = item[3];
+  console.log(index + '.' + desc, words);
+  Object.keys(styles).forEach(style => {
+    const result = pinyin(words, Object.assign({}, {style: pinyin[style]}, options));
+    const passed = JSON.stringify(result) === JSON.stringify(styles[style]);
+    if (passed) {
+      console.log(style, '通过'.green);
+    } else {
+      console.log(style, 'expected:'.green, styles[style], 'recieved:'.red, result, '不通过'.red);
+    }
   });
 });
--- __test__/total.test.js
+++ __test__/total.test.js
@@ -1,11 +1,28 @@
+var colors = require( 'colors');
 const pinyin = require('../src/index');
 const testDict = require('../data/phrases_test.dict.js');
 
 
-describe('使用cc-cedict生成的字典测试', () => {
-  Object.keys(testDict).forEach((word) => {
-    test(word, () => {
-      expect(pinyin(word, {style: pinyin.STYLE_TONE_NUM})).toEqual(testDict[word].replace(/,/g,'，').split(' '));
-    });
-  });
+// describe('使用cc-cedict生成的字典测试', () => {
+//   Object.keys(testDict).forEach((word) => {
+//     test(word, () => {
+//       expect(pinyin(word, {style: pinyin.STYLE_TONE_NUM})).toEqual(testDict[word].replace(/,/g,'，').split(' '));
+//     });
+//   });
+// });
+
+let count = 0;
+Object.keys(testDict).forEach((word) => {
+  const result = pinyin(word, Object.assign({}, {style: pinyin.STYLE_TONE_NUM})).join(' ');
+  const expect = testDict[word].replace(/,/g,'，');
+  const passed = result === expect;
+
+  if (passed) {
+    // console.log(word, '通过'.green);
+  } else {
+    count++;
+    console.log(word, 'expected: '.green, expect, 'recieved: '.red, result, '不通过'.red);
+  }
 });
+
+console.log(count/Object.keys(testDict).length);
